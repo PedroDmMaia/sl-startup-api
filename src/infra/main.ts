e3e68be89b2c63fbd80fea7c6821ef858cd05f61
Pedro <pedrodelmonico@hotmail.com>
2025-01-14 00:12:00 -0300

    feat: adding the public and private key to .env file
    -fix: adjusting the code to use the env module

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
+import { EnvService } from './env/envService'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  const configService = app.get(ConfigService)
-  const port = configService.get('PORT')
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
 
   await app.listen(port)
 }
